c59daea98cd177bdf172c188b6bab4e3f7eff9de
korvalanchik <korvalan@yahoo.com>
2023-09-15 21:22:21 +0300

    Word counting and sorting 1.0

---
+++ src/main/java/Demo.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        Wordmatch inp = new Wordmatch();
+        inp.readFile();
+    }
+}
---
+++ src/main/java/Wordmatch.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Wordmatch {
+    public void readFile() throws IOException {
+        String[] words = readBuffer().split(" ");
+        HashMap<String, Integer> wordAndCount = new HashMap<>();
+        for(String word: words) {
+            if(wordAndCount.containsKey(word)) {
+                wordAndCount.put(word, wordAndCount.get(word) + 1);
+            } else {
+                wordAndCount.put(word, 1);
+            }
+        }
+        TreeMap<ObjectMap, String> sortingCount = new TreeMap<>(comparator);
+        for (HashMap.Entry<String, Integer> entry : wordAndCount.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            ObjectMap objectMap = new ObjectMap(value, key);
+            sortingCount.put(objectMap, key);
+        }
+        Iterator<Map.Entry<ObjectMap, String>> itr = sortingCount.entrySet().iterator();
+        while(itr.hasNext()) {
+            Map.Entry<ObjectMap, String> entry =  itr.next();
+            ObjectMap obj = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(value + " " + obj.toString());
+        }
+    }
+
+    private static String readBuffer() throws IOException {
+        String userDirectory = new File("").getAbsolutePath();
+        BufferedReader reader = new BufferedReader( new FileReader (userDirectory +"/src/main/resources/words.txt"));
+        String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        while((line = reader.readLine()) != null ) {
+            stringBuilder.append(line);
+            stringBuilder.append(" ");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    Comparator<ObjectMap> comparator = new Comparator<ObjectMap>() {
+
+        @Override
+        public int compare(ObjectMap obj1, ObjectMap obj2) {
+            return obj2.getCommonValue().compareTo(obj1.getCommonValue());
+        }
+    };
+
+    private static class ObjectMap {
+        Integer value;
+        String key;
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+
+        public ObjectMap(Integer value, String key) {
+            this.value = value;
+            this.key = key;
+        }
+
+        public String getCommonValue() {
+            return value + key.toString();
+        }
+    }
+
+}
+
